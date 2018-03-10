7a09fc181e25599e23cfe39b38c434c8240d509b
Locke <lockereed@gmail.com>
2018-03-09 23:47:26 -0700

    crap

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ fetch('https://lit-fortress-6467.herokuapp.com/object')
     console.log(apiData)
   })
 
+//document.getElementById("track").style.zIndex = "0";
+document.getElementsByClassName('album_container').style.zIndex = "1";
+document.getElementById("track").style.right = "-100px";
+
+
+
+
 
 /*    let movies = apiData.Search;
 
